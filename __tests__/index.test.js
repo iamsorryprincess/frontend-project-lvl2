5727a50065bec62d128df06810de29fa16c94b29
iamsorryprincess <nickdollars@yandex.ru>
2020-03-07 10:24:47 +0300

    step 4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,20 @@
+import diff from '../src/index.js';
+
+const dir = `${__dirname}/fixtures`;
+
 test('test', () => {
-  expect(1 + 4).toBe(5);
+  const filename1 = `${dir}/before.json`;
+  const filename2 = `${dir}/after.json`;
+  const expected = [
+    '{',
+    '     host: hexlet.io',
+    '   - timeout: 50',
+    '   + timeout: 20',
+    '   - proxy: 123.234.53.22',
+    '   - follow: false',
+    '   + verbose: true',
+    '}'
+  ];
+  const result = diff(filename1, filename2);
+  expect(result.join(' ')).toBe(expected.join(' '));
 });
