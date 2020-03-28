819f4b274ef97079afff48574f6a8f03788b66ed
iamsorryprincess <nickdollars@yandex.ru>
2020-03-28 13:08:48 +0300

    refactoring tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,34 +7,22 @@ const getRelativeFilePath = (filename) => {
   return path.join(commonFixturePath, filename);
 };
 
-const normalize = str => str.split('').sort().join();
-
-const check = ({ fileBefore, fileAfter, expected, format }) => {
-  const expectedFile = fs.readFileSync(getRelativeFilePath(expected), 'utf-8');
-  const result = diff(`${__dirname}/__fixtures__/${fileBefore}`, `${__dirname}/__fixtures__/${fileAfter}`, format);
-  if (format !== 'json') {
-    expect(normalize(result)).toBe(normalize(expectedFile));
-  } else {
-    expect(
-      JSON.stringify(JSON.parse(diff(`${__dirname}/__fixtures__/${fileBefore}`, `${__dirname}/__fixtures__/${fileAfter}`, format))))
-        .toBe(JSON.stringify(JSON.parse(expectedFile)));
-  }
+const check = ({ format, extension }) => {
+  const expected = fs.readFileSync(getRelativeFilePath(`${format}.txt`), 'utf-8');
+  const before = getRelativeFilePath(`before.${extension}`);
+  const after = getRelativeFilePath(`after.${extension}`);
+  const actual = diff(before, after, format);
+  expect(actual).toBe(expected);
 };
 
 test.each`
-  fileBefore           | fileAfter            | expected                               | format      | extension
-  ${'before.json'}     | ${'after.json'}      | ${'jsonStringBeforeAfterExpected.txt'} | ${'string'} | ${'.json'}
-  ${'before.yml'}      | ${'after.yml'}       | ${'jsonStringBeforeAfterExpected.txt'} | ${'string'} | ${'.yml'}
-  ${'before.ini'}      | ${'after.ini'}       | ${'jsonStringBeforeAfterExpected.txt'} | ${'string'} | ${'.ini'}
-  ${'after.json'}      | ${'before.json'}     | ${'jsonStringAfterBeforeExpected.txt'} | ${'string'} | ${'.json'}
-  ${'after.yml'}       | ${'before.yml'}      | ${'jsonStringAfterBeforeExpected.txt'} | ${'string'} | ${'.yml'}
-  ${'after.ini'}       | ${'before.ini'}      | ${'jsonStringAfterBeforeExpected.txt'} | ${'string'} | ${'.ini'}
-  ${'before.json'}     | ${'after.json'}      | ${'plainBeforeExpected.txt'}           | ${'plain'}  | ${'.json'}
-  ${'before.yml'}      | ${'after.yml'}       | ${'plainBeforeExpected.txt'}           | ${'plain'}  | ${'.yml'}
-  ${'before.ini'}      | ${'after.ini'}       | ${'plainBeforeExpected.txt'}           | ${'plain'}  | ${'.ini'}
-  ${'after.json'}      | ${'before.json'}     | ${'plainAfterExpected.txt'}            | ${'plain'}  | ${'.json'}
-  ${'after.yml'}       | ${'before.yml'}      | ${'plainAfterExpected.txt'}            | ${'plain'}  | ${'.yml'}
-  ${'after.ini'}       | ${'before.ini'}      | ${'plainAfterExpected.txt'}            | ${'plain'}  | ${'.ini'}
-  ${'before.json'}     | ${'after.json'}      | ${'jsonExpected.txt'}                  | ${'json'}   | ${'.json'}
-  ${'before.yml'}      | ${'after.yml'}       | ${'jsonExpected.txt'}                  | ${'json'}   | ${'.yml'}
+  format      | extension
+  ${'string'} | ${'json'}
+  ${'string'} | ${'yml'}
+  ${'string'} | ${'ini'}
+  ${'plain'}  | ${'json'}
+  ${'plain'}  | ${'yml'}
+  ${'plain'}  | ${'ini'}
+  ${'json'}   | ${'json'}
+  ${'json'}   | ${'yml'}
 `('Check rendering for $format format for $extension file', check);
